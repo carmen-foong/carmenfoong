133afbdb2521b055a817718d67afde2f03d968b8
scriptwes1337 <hello@wneoh.com>
2025-04-05 20:54:43 +0800

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website/", // Add this line (replace with your actual repo name)
+  base: "/carmenfoong/", // Add this line (replace with your actual repo name)
 });
